4bfa3a27e451b7816e1bbde3228c5cf5832e5cc3
Mike Zornek <mike@clickablebliss.com>
2015-05-07 16:28:46 -0400

    Adding table with user's choice of voices to be used.

--- SpeakLine/MainWindowController.swift
+++ SpeakLine/MainWindowController.swift
@@ -7,10 +7,12 @@ class MainWindowController: NSWindowController {
     @IBOutlet weak var textField: NSTextField!
     @IBOutlet weak var speakButton: NSButton!
     @IBOutlet weak var stopButton: NSButton!
+    @IBOutlet weak var tableView: NSTableView!
     
     //MARK: - Properties
     
     let speechSynth = NSSpeechSynthesizer()
+    var voices = NSSpeechSynthesizer.availableVoices() as! [String]
     var isStarted = false {
         didSet {
             updateButtons()
@@ -27,6 +29,10 @@ class MainWindowController: NSWindowController {
         super.windowDidLoad()
         updateButtons()
         speechSynth.delegate = self
+        for voice in voices {
+            println(voiceNameForIdentifier(voice)!)
+        }
+        selectDefaultVoice()
     }
     
     //MARK: - Actions
@@ -58,6 +64,23 @@ class MainWindowController: NSWindowController {
         }
     }
     
+    func voiceNameForIdentifier(identifier: String) -> String? {
+        if let attributes = NSSpeechSynthesizer.attributesForVoice(identifier) {
+            return attributes[NSVoiceName] as? String
+        } else {
+            return nil
+        }
+    }
+    
+    func selectDefaultVoice() {
+        let defaultVoice = NSSpeechSynthesizer.defaultVoice()
+        if let defaultRow = find(voices, defaultVoice) {
+            let indices = NSIndexSet(index: defaultRow)
+            tableView.selectRowIndexes(indices, byExtendingSelection: false)
+            tableView.scrollRowToVisible(defaultRow)
+        }
+    }
+    
 }
 
 extension MainWindowController : NSSpeechSynthesizerDelegate {
@@ -72,6 +95,30 @@ extension MainWindowController : NSWindowDelegate {
     
     func windowShouldClose(sender: AnyObject) -> Bool {
         return !isStarted
+    }
+    
+}
+
+extension MainWindowController: NSTableViewDataSource, NSTableViewDelegate {
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return voices.count
+    }
+    
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        let voice = voices[row]
+        let voiceName = voiceNameForIdentifier(voice)
+        return voiceName
+    }
+    
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        let row = tableView.selectedRow
+        if row == -1 {
+            speechSynth.setVoice(nil)
+        } else {
+            let voice = voices[row]
+            speechSynth.setVoice(voice)
+        }
     }
     
 }
